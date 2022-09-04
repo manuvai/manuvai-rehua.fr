0ea17ef6f900cb6d7d343106b5443c9b3bbf58c5
manuvai <rm90266@gmail.com>
2022-09-04 20:33:51 +0200

    feat: Ajout de la table des expériences

---
+++ admin/database/migrations/2022_09_04_182924_create_cursuses_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCursusesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cursuses', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('company');
+            $table->string('place');
+            $table->date('start_date');
+            $table->date('end_date');
+            $table->longtext('description');
+            $table->enum('type', ['job', 'internship', 'school']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cursuses');
+    }
+}

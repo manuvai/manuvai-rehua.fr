66b5f5d2f5f0358f30b639bd40c7fe240c31e7d8
manuvai <rm90266@gmail.com>
2022-09-17 02:52:09 +0200

    feat: Ajout de l'affichage de la page Introduction

--- front/src/app/home/introduction/introduction.component.ts
+++ front/src/app/home/introduction/introduction.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpService } from 'src/app/http.service';
 
 @Component({
   selector: 'app-introduction',
@@ -6,10 +7,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./introduction.component.scss']
 })
 export class IntroductionComponent implements OnInit {
+  settings = {
+    captcha_prv_key: "",
+    captcha_pub_key: "",
+    cv_file_path: "",
+    email: "",
+    hero_image_path: "",
+    introduction_text: "",
+    linkedin_badge: "",
+    mobile: "",
+  };
 
-  constructor() { }
+  constructor(private httpService: HttpService) { }
 
   ngOnInit(): void {
+    this.httpService
+      .getParameters()
+      .subscribe((response: any) => {
+        this.settings = response;
+        this.settings.introduction_text = "<h1 class=\"display-5 fw-bold lh-1 mb-3\">Bonjour, Je suis Manuvai REHUA</h1>" + this.settings.introduction_text
+        this.settings.introduction_text+= `
+        <div class="d-grid gap-2 d-md-flex justify-content-md-start">
+          <a class="btn btn-primary btn-lg px-4 me-md-2" download="" target="_blank" href="{{'http://localhost:8000/storage/' + settings.cv_file_path}}">T&eacute;l&eacute;charger le CV</a>
+          <a class="btn btn-outline-secondary btn-lg px-4" href="#contact">Envoyer un message</a>
+        </div>
+        `
+      });
+
   }
 
 }

53a64a8f795b2b65bbb849f6ce3bafee53db6368
manuvai <rm90266@gmail.com>
2022-09-20 21:36:57 +0200

    fix: Correction de l'affichage des données des expériences

--- front/src/app/home/cursuses/cursuses.component.ts
+++ front/src/app/home/cursuses/cursuses.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpService } from 'src/app/http.service';
 
 @Component({
   selector: 'app-cursuses',
@@ -7,9 +8,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CursusesComponent implements OnInit {
 
-  constructor() { }
+  experiences = [{
+    name: '',
+    company: '',
+    place: '',
+    start_date: '',
+    end_date: '',
+    description: '',
+    type: '',
+  }]
+  constructor(private httpService: HttpService) { }
 
+  getIcon(type: string): string {
+    return((type == 'job') ? 'briefcase' : (((type == 'school') ? 'book' : 'clipboard')))
+  }
   ngOnInit(): void {
+    this.httpService
+      .getCursuses()
+      .subscribe((response: any) => {
+        this.experiences = response.data;
+      });
   }
 
 }

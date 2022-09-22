6b20d730f509630a9869542c5f1008881b4313dd
manuvai <rm90266@gmail.com>
2022-09-22 22:15:01 +0200

    feat: Ajout de contact

--- front/src/app/home/contact/contact.component.ts
+++ front/src/app/home/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpService } from 'src/app/http.service';
 
 @Component({
   selector: 'app-contact',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
 
-  constructor() { }
+  linkedin_badge = '';
+
+  constructor(private httpService: HttpService) { }
 
   ngOnInit(): void {
+    this.httpService
+      .getParameters()
+      .subscribe((response: any) => {
+        this.linkedin_badge = response.linkedin_badge
+      })
   }
 
 }

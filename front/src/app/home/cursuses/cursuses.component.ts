4fa59f569031a904853e6d81a25411237819493a
manuvai <rm90266@gmail.com>
2022-09-29 21:32:38 +0200

    feat: Amélioration de l'affichage des dates

--- front/src/app/home/cursuses/cursuses.component.ts
+++ front/src/app/home/cursuses/cursuses.component.ts
@@ -22,6 +22,24 @@ export class CursusesComponent implements OnInit {
   getIcon(type: string): string {
     return((type == 'job') ? 'briefcase' : (((type == 'school') ? 'book' : 'clipboard')))
   }
+
+  getDateLabel(exp: any): string {
+
+    let start_date = new Date(exp.start_date);
+    let month1 = start_date.toLocaleString('default', { month: 'long' });
+    month1 = month1.substr( 0, 1 ).toUpperCase() + month1.substr( 1 )
+    const year1 = start_date.getFullYear();
+    
+    if (!exp.end_date) {
+      return `${month1} ${year1} - Aujourd'hui`
+    }
+    let end_date = new Date(exp.end_date);
+    let month2 = end_date.toLocaleString('default', { month: 'long' });
+    month2 = month2.substr( 0, 1 ).toUpperCase() + month2.substr( 1 )
+    const year2 = end_date.getFullYear();
+    return `${month1} ${year1} - ${month2} ${year2}`
+  }
+
   ngOnInit(): void {
     this.httpService
       .getCursuses()
